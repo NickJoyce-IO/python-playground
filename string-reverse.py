1ce3f9a1f230424d5f8ec5f2c96d731f6cbebe34
Nick Joyce <brawlyrox@gmail.com>
2020-02-12 21:00:28 +0000

    Add code for reversing a string

---
+++ string-reverse.py
@@ -0,0 +1,8 @@
+# Test.expect(solution('world') == 'dlrow')
+# Test.expect(solution('hello') == 'olleh')
+# Test.expect(solution('') == '')
+# Test.expect(solution('h') == 'h')
+
+def solution(string):
+    # reverse string using array bracket notation
+    return string[::-1]
